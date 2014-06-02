33aed95651fcd8afe34d1e13183b9a34a9cc17ce
Dave Augustus <dave.augustus@bottlerocketapps.com>
2014-06-02 20:18:55 +0000

    added support for ssl

--- manifests/frontend.pp
+++ manifests/frontend.pp
@@ -59,6 +59,7 @@ define haproxy::frontend (
   $ports,
   $ipaddress        = [$::ipaddress],
   $mode             = undef,
+	$sslmode					=	undef,
   $collect_exported = true,
   $options          = {
     'option'  => [
